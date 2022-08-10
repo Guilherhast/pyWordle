38bc7e21e6ab31226b3a5f1d66c71845fcce0ee4
Guilherme Hastenretier Ferreira <instrutores.bhserra@ctrlplay.online>
2022-08-10 12:52:33 -0300

    ConfigServer: Mk_db_folder

--- configServer.py
+++ configServer.py
@@ -1,6 +1,15 @@
+import os
+
+from globals import *
+
+#Functions
+#OS
+def make_db_dir(db_dir):
+    os.makedirs(db_dir,exist_ok=True)
+
 #System
 def main():
-    print("Hello from configServer")
+    make_db_dir(db_folder)
 
 if __name__ == '__main__':
     main()
