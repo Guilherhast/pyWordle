fa347d471bf680e3faac104c2b132881648c6617
Guilherme Hastenretier Ferreira <instrutores.bhserra@ctrlplay.online>
2022-08-10 12:07:41 -0300

    Globals: Server

--- globals.py
+++ globals.py
@@ -10,6 +10,17 @@ db_all  = os.path.join(db_folder, 'db.txt')
 db_words  = os.path.join(db_folder, 'words.txt')
 db_start  = os.path.join(db_folder, 'time.txt')
 
+#Server
+port = 8000
+hostname = '0.0.0.0'
+
+server_cfg = (hostname,port)
+
+public_folder = os.path.join(root_dir,"public")
+
+api_re = '/api.*'
+param_re = 'day=(\\d+)'
+
 #Print variables
 if __name__ == '__main__':
     #Files
@@ -18,3 +29,8 @@ if __name__ == '__main__':
     print(db_all)
     print(db_words)
     print(db_start)
+    #Server
+    print(server_cfg)
+    print(public_folder)
+    print(api_re)
+    print(param_re)
