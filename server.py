77693c8b40e8a8da7c20c3326ceff83e2eec211d
Guilherme Hastenretier Ferreira <instrutores.bhserra@ctrlplay.online>
2022-08-10 12:36:08 -0300

    Server: Retornar palavra e public v0.0.3

--- server.py
+++ server.py
@@ -1,16 +1,25 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
+import re
+import functools
+
+from http.server import SimpleHTTPRequestHandler, HTTPServer
 
 from globals import *
 
-class Server(BaseHTTPRequestHandler):
+class Server(SimpleHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html; charset=utf-8")
-        self.end_headers()
+        if re.match(api_re,self.path):
+            self.send_response(200)
+            self.send_header("Content-type", "text/html; charset=utf-8")
+            self.send_header("Cache-Control", "no-cache, no-store, must-revalidate")
+            self.send_header("Pragma", "no-cache")
+            self.send_header("Expires", "0")
+            self.end_headers()
 
-        word = get_word(self.path)
+            word = get_word(self.path)
 
-        self.wfile.write(bytes(word, "utf-8"))
+            self.wfile.write(bytes(word, "utf-8"))
+        else:
+            SimpleHTTPRequestHandler.do_GET(self)
 
 #Functions
 #Calculation
@@ -19,7 +28,8 @@ def get_word(path=''):
 
 #System
 def main():
-    webServer = HTTPServer(server_cfg, Server)
+    handler = functools.partial(Server,directory=public_folder)
+    webServer = HTTPServer(server_cfg, handler)
     print("Server started http://%s:%s" % server_cfg)
 
     try:
