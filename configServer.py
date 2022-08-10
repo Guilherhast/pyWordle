0d1d5242753e2eee2b40fdc9984425f66ef21151
Guilherme Hastenretier Ferreira <instrutores.bhserra@ctrlplay.online>
2022-08-10 12:55:17 -0300

    ConfigServer: Download_db

--- configServer.py
+++ configServer.py
@@ -1,4 +1,5 @@
 import os
+import urllib.request
 
 from globals import *
 
@@ -7,9 +8,15 @@ from globals import *
 def make_db_dir(db_dir):
     os.makedirs(db_dir,exist_ok=True)
 
+#Network
+def download_db(url, file_to):
+    urllib.request.urlretrieve(url,file_to)
+
 #System
 def main():
-    make_db_dir(db_folder)
+    #make_db_dir(db_folder)
+    download_db(url_all_words,db_all)
+    print("Done")
 
 if __name__ == '__main__':
     main()
