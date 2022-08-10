e33c50dec55bcf9a248ed1ba433d3088343985b5
Guilherme Hastenretier Ferreira <instrutores.bhserra@ctrlplay.online>
2022-08-10 12:18:58 -0300

    Server: Retornar palavra v0.0.2

--- server.py
+++ server.py
@@ -5,16 +5,20 @@ from globals import *
 class Server(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
-        self.send_header("Content-type", "text/html")
+        self.send_header("Content-type", "text/html; charset=utf-8")
         self.end_headers()
 
-        self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
-        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
-        self.wfile.write(bytes("<body>", "utf-8"))
-        self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
-        self.wfile.write(bytes("</body></html>", "utf-8"))
+        word = get_word(self.path)
 
-if __name__ == "__main__":
+        self.wfile.write(bytes(word, "utf-8"))
+
+#Functions
+#Calculation
+def get_word(path=''):
+    return 'Berro'
+
+#System
+def main():
     webServer = HTTPServer(server_cfg, Server)
     print("Server started http://%s:%s" % server_cfg)
 
@@ -25,3 +29,7 @@ if __name__ == "__main__":
 
     webServer.server_close()
     print("Server stopped.")
+
+#Run
+if __name__ == "__main__":
+    main()
